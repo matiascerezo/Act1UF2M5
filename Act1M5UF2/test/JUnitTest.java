8b50d2e3d1a5faf3eff6ab6585a274b472000bca
matiascerezo <maatiascerezo@gmail.com>
2016-10-21 21:24:43 +0200

    Update JUnitTest.java

--- Act1M5UF2/test/JUnitTest.java
+++ Act1M5UF2/test/JUnitTest.java
@@ -6,6 +6,7 @@
 import myutils.MyUtils;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
