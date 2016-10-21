a8becfae6d99f93233864f87a38183f03ca82462
matiascerezo <maatiascerezo@gmail.com>
2016-10-21 17:42:08 +0200

    Update Test.java

--- Act1M5UF2/src/Prueba/Test.java
+++ Act1M5UF2/src/Prueba/Test.java
@@ -7,7 +7,7 @@ public class Test {
 
     public static void main(String[] args) {
              
-        System.out.println(MyUtils.cadenaInvertida("hola"));
+        System.out.println(MyUtils.cadenaInvertida("Matias"));
         System.out.println(MyUtils.edad(29,9,996));
         System.out.println(MyUtils.factorial(-3));
     }
